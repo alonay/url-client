908cb63ccdca134d5a708e9df3a74b69004dee49
Alona <github email address>
2018-04-08 20:38:32 -0400

    Added leaderboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import logo from './logo.svg'
 import Maker from "./maker"
+import LeaderBoard from "./leaderBoard"
+
 class App extends Component {
-  getLinks = () => {
-    fetch('http://localhost:3000/links')
-       .then(res => res.json())
-       .then(json => console.log(json))
-
-   }
   render() {
-    {this.getLinks()}
     return (
-
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -22,8 +16,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Maker/>
+        <LeaderBoard/>
       </div>
-    );
+    )
   }
 }
 
--- src/leaderBoard.js
+++ src/leaderBoard.js
@@ -1,5 +1,44 @@
 import React from "react"
 
-class LeaderBoard extends React.Component{
-  
+class LeaderBoard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      links: []
+    }
+  }
+
+  componentDidMount() {
+    this.getLinks()
+  }
+
+  showLeaders = (json) => {
+    this.setState({
+      links: json,
+    })
+  }
+
+  getLinks = () => {
+    fetch('http://localhost:3000/links')
+      .then(res => res.json())
+      .then(json => this.showLeaders(json))
+  }
+
+
+  render() {
+    return (
+      <ol>
+        {
+          this.state.links.map((link, index) => {
+            return (
+              <li>{index + 1} - {link.long}</li>
+            )
+          })
+        }
+      </ol>
+    )
+  }
 }
+
+export default LeaderBoard
